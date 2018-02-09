2b6d13c69baa15b65830daba6637e02e31f1d8fc
Isaac I.Y. Saito <130s@2000.jukuin.keio.ac.jp>
2018-02-08 16:38:32 -0800

    [RosPack] Add a license introspection feature.
    ```
    $ roslicenses --help
    usage: roslicenses [-h] [--implicit IMPLICIT] [--sortbylicense SORTBYLICENSE]
                       [--save_result SAVE_RESULT]
                       [--prefix_savefile PREFIX_SAVEFILE]
                       pkg_name
    
    List the licenses of all ROS dependency for the given package..
    
    positional arguments:
      pkg_name              Name of the package you want to get the all licenses
                            of.
    
    optional arguments:
      -h, --help            show this help message and exit
      --implicit IMPLICIT   If True licenses from the implicit dependencies are
                            sought (i.e. non-immediate dependencies).
      --sortbylicense SORTBYLICENSE
                            If False, the result will be sorted per package name.
      --save_result         If True save the result to a text file with a prefix
                            specified by --prefix_savefile.
      --prefix_savefile PREFIX_SAVEFILE
                            Prefix of the output file in an absolute full path
                            style (default /tmp/licenses). Either absolute or
                            relative path can be passed
    
    $ ./scripts/roslicenses roscpp --save_result True
    List of licenses is saved in /tmp/licenses_roscpp-1.12.12.yml
    $ more /tmp/licenses_roscpp-1.12.12.yml
    - !!python/tuple
      - BSD
      - - rosconsole
        - catkin
        - cmake_modules
        - rospack
        - rosbuild
        - gencpp
        - roslib
        - geneus
        - roscpp_serialization
        - roscpp_traits
        - message_generation
        - roslang
        - genlisp
        - genpy
        - std_msgs
        - message_runtime
        - rostime
        - rosunit
        - rosgraph_msgs
        - genmsg
        - cpp_common
    - !!python/tuple
      - LGPL-2.1
      - - xmlrpcpp
    - !!python/tuple
      - Apache 2.0
      - - gennodejs
    ```
    
    [get_licenses] Save in better yaml format.
    
    [get_licenses] Move to RosPack class where get_rosdeps is available.
    This is needed for a forthcoming change that introduces getting system pkgs to the license result.
    
    [get_licenses] license comparision feature.

--- src/rospkg/rospack.py
+++ src/rospkg/rospack.py
@@ -30,6 +30,7 @@
 # ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
+from collections import defaultdict
 import os
 from threading import Lock
 
@@ -390,6 +391,51 @@ class RosPack(ManifestManager):
                 return os.path.basename(d)
             else:
                 d = os.path.dirname(d)
+
+    def get_licenses(self, name, implicit=True, sortbylicense=True):
+        """
+        @summary: Return a list of licenses the packages the given package declares
+                             dependency on.
+        @return Dictionary. By default dict of license name and package name(s).
+                       Note: For the packages that declare multiple licenses, those licenses
+                       are returned in a single string with each license separated by comma.
+                       E.g. if your package declares BSD and LGPL in separate tags in
+                       package.xml, the returned key will look like "BSD, LGPL".
+        @rtype { k, [d] }
+        @raise ResourceNotFound
+        """
+        license_dict = defaultdict(list)
+        license_list = []
+
+        # Unlike get_depends, the licenses of the given package itself needs added.
+        manifest_self = self.get_manifest(name)
+        license_list.append((name, manifest_self.license))
+
+        # Get licenses from depended packages
+        try:
+            pkgnames_dep = self.get_depends(name, implicit)
+        except ResourceNotFound as e:
+            raise e
+        for pkgname_dep in pkgnames_dep:
+            manifest = self.get_manifest(pkgname_dep)
+            license_list.append((pkgname_dep, manifest.license))
+
+        # Traverse for Non-ROS, system packages
+        try:
+            pkgnames_rosdep = self.get_rosdeps(name, implicit)
+        except ResourceNotFound as e:
+            raise e
+        for pkgname_rosdep in pkgnames_rosdep:
+            license_list.append((
+                pkgname_rosdep, "(System package. License not automatically detected)"))
+
+        for pkgname, license_name in license_list:
+            if not sortbylicense:
+                license_dict[pkgname].append(license_name)
+            else:
+                license_dict[license_name].append(pkgname)
+        licenses = license_dict.items()
+        return dict(licenses)
 
 
 class RosStack(ManifestManager):
